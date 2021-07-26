c6e34a31a30d4b6ee32063e4faa386fef284185f
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-07-26 16:22:50 +0100

    Add comment section

--- laravel-post/resources/views/components/post.blade.php
+++ laravel-post/resources/views/components/post.blade.php
@@ -10,14 +10,15 @@
             {{$post->created_at->diffForHumans()}}
         </span>
         <p class="mb-2">{{$post->body}}</p>
- 
+        
+       
         @can('delete',$post)
             <form action="{{route('posts.destroy',$post)}}" method="post" class="mr-1 float-right">
                 @csrf
                 @method('DELETE')
                 <button type="submit"><i class="fas fa-trash text-red-500 text-xl  cursor-pointer"></i></button>
             </form>
-        @endcan 
+        @endcan
 
         @can('update',$post)
         <form action="{{route('posts.edit',$post)}}" method="post" class="mr-1 float-right mr-4">
@@ -27,25 +28,32 @@
         </form>
         @endcan   
 
-        <div class="flex items-center">
-        @auth
+        <div class="flex item-center">
+            @auth
 
-            @if(!$post->likedBy(auth()->user()))
-                <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
-                    @csrf
-                    <button type="submit"><i class="far fa-thumbs-up text-current text-2xl cursor-pointer"></i></button>
-                </form>
-            @else
-                <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit"><i class="fas fa-thumbs-up text-blue-500 text-2xl cursor-pointer"></i></button>
-                </form>
-            @endif
+                @if(!$post->likedBy(auth()->user()))
+                    <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
+                        @csrf
+                        <button type="submit"><i class="far fa-thumbs-up text-current text-2xl cursor-pointer"></i></button>
+                    </form>
+                @else
+                    <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"><i class="fas fa-thumbs-up text-blue-500 text-2xl cursor-pointer"></i></button>
+                    </form>
+                @endif
 
-           
-        @endauth
-        <span>{{$post->likes->count()}} {{Str::plural('like',$post->likes->count())}}</span>
+            
+            @endauth
+            <span>{{$post->likes->count()}} {{Str::plural('like',$post->likes->count())}}</span>
+        @can('update',$post)
+        <form action="{{route('posts.comment',$post)}}" method="post" class="mr-2 ml-10">
+            @csrf
+            <button type="submit"><i class="far fa-comment text-gray-700 text-xl cursor-pointer"></i>   Comment</button>
+
+        </form>
+        @endcan 
         </div>
 
     </div> @endif
--- laravel-post/routes/web.php
+++ laravel-post/routes/web.php
@@ -31,7 +31,10 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts?id={post:id}', [PostController::class, 'update'])->name('posts.edit');
+Route::get('/posts?id={post:id}', [PostController::class, function () {
+}])->name('posts.edit');
+Route::post('/posts/{post}', [PostController::class, function () {
+}])->name('posts.comment');
 
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.like');
 Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.like');
