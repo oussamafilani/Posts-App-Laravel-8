3316143f77283bfc9d0ec5b603c34a1bbbb78315
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-07-28 09:28:08 +0100

    Admin delete admin permissions done

--- laravel-post/resources/views/components/post.blade.php
+++ laravel-post/resources/views/components/post.blade.php
@@ -1,107 +1,128 @@
 @props(['post' => $post])
 
 <div>
-    @if($post->id !=  request()->get('id')  )
+    @if ($post->id != request()->get('id'))
 
-    <div class="mb-4 bg-gray-50 p-5 rounded-lg relative ">
-        {{-- <div class="absolute right-0 top-0"><i class="fas fa-ellipsis-h cursor-pointer mr-3"></i></div> --}}
+        <div class="mb-4 bg-gray-50 p-5 rounded-lg relative ">
+            {{-- <div class="absolute right-0 top-0"><i class="fas fa-ellipsis-h cursor-pointer mr-3"></i></div> --}}
 
-        <a href="{{route('users.posts',$post->user)}}" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">
-            {{$post->created_at->diffForHumans()}}
-        </span>
-        <p class="mb-2 p-5">{{$post->body}}</p>
-        
-       
-        @can('delete',$post)
-            <form action="{{route('posts.destroy',$post)}}" method="post" class="mr-1 float-right">
-                @csrf
-                @method('DELETE')
-                <button type="submit"><i class="fas fa-trash text-red-500 text-xl  cursor-pointer"></i></button>
-            </form>
-        @endcan
+            <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a> <span
+                class="text-gray-600 text-sm">
+                {{ $post->created_at->diffForHumans() }}
+            </span>
+            <p class="mb-2 p-5">{{ $post->body }}</p>
 
-        @can('update',$post)
-        <form action="{{route('posts.edit',$post)}}" method="post" class="mr-1 float-right mr-4">
-            @csrf
-            @method('GET')
-            <button type="submit"><i class="fas fa-edit text-gray-700 text-xl  cursor-pointer"></i></button>
-        </form>
-        @endcan   
+            {{-- start delete post --}}
+            @if (auth()->user()->role == 1)
 
-        <div class="flex item-center">
-            @auth
-
-                @if(!$post->likedBy(auth()->user()))
-                    <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
-                        @csrf
-                        <button type="submit"><i class="far fa-thumbs-up text-current text-2xl cursor-pointer"></i></button>
-                    </form>
-                @else
-                    <form action="{{route('posts.like',$post)}}" method="post" class="mr-1">
+                <form action="{{ route('posts.destroy', $post) }}" method="post" class="mr-1 float-right">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"><i class="fas fa-trash text-red-500 text-xl  cursor-pointer"></i></button>
+                </form>
+            @else
+                @can('delete', $post)
+                    <form action="{{ route('posts.destroy', $post) }}" method="post" class="mr-1 float-right">
                         @csrf
                         @method('DELETE')
-                        <button type="submit"><i class="fas fa-thumbs-up text-blue-500 text-2xl cursor-pointer"></i></button>
+                        <button type="submit"><i class="fas fa-trash text-red-500 text-xl  cursor-pointer"></i></button>
                     </form>
-                @endif
+                @endcan
+            @endif
+            {{-- end delete post --}}
 
-            
-            @endauth
-            <span>{{$post->likes->count()}} {{Str::plural('like',$post->likes->count())}}</span>
-        <form action="{{route('posts.comment',$post)}}" method="post" class="mr-2 ml-10">
-            @csrf
-            @method('GET')
-            <button type="submit"><i class="far fa-comment text-gray-700 text-xl cursor-pointer"></i>   {{$post->comments->count()}}</button>
 
-        </form>
-   
-        </div>
+            @can('update', $post)
+                <form action="{{ route('posts.edit', $post) }}" method="post" class="mr-1 float-right mr-4">
+                    @csrf
+                    @method('GET')
+                    <button type="submit"><i class="fas fa-edit text-gray-700 text-xl  cursor-pointer"></i></button>
+                </form>
+            @endcan
 
-        {{-- start Comment section --}}
-        <form action="{{route('posts.comment',$post)}}" method="POST" class="my-4 ">
-            @csrf
-            <div class="mb-4">
-                <label for="comment" class="sr-only">Comment</label>
-                <textarea name="comment" id="comment" cols="30" rows="1" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('comment') border-red-500 @enderror" placeholder="write a comment ..."></textarea>
-                
-                @error('comment')
-                <div class="text-red-500 mt-2 text-sm">
-                    {{ $message }}
+            <div class="flex item-center">
+                @auth
+
+                    @if (!$post->likedBy(auth()->user()))
+                        <form action="{{ route('posts.like', $post) }}" method="post" class="mr-1">
+                            @csrf
+                            <button type="submit"><i
+                                    class="far fa-thumbs-up text-current text-2xl cursor-pointer"></i></button>
+                        </form>
+                    @else
+                        <form action="{{ route('posts.like', $post) }}" method="post" class="mr-1">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"><i
+                                    class="fas fa-thumbs-up text-blue-500 text-2xl cursor-pointer"></i></button>
+                        </form>
+                    @endif
+
+
+                @endauth
+                <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                <form action="{{ route('posts.comment', $post) }}" method="post" class="mr-2 ml-10">
+                    @csrf
+                    @method('GET')
+                    <button type="submit"><i class="far fa-comment text-gray-700 text-xl cursor-pointer"></i>
+                        {{ $post->comments->count() }}</button>
+
+                </form>
+
+            </div>
+
+            {{-- start Comment section --}}
+            <form action="{{ route('posts.comment', $post) }}" method="POST" class="my-4 ">
+                @csrf
+                <div class="mb-4">
+                    <label for="comment" class="sr-only">Comment</label>
+                    <textarea name="comment" id="comment" cols="30" rows="1"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('comment') border-red-500 @enderror"
+                        placeholder="write a comment ..."></textarea>
+
+                    @error('comment')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-            
-            <div>
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Reply</button>
-            </div>
-        </form>
 
-        {{-- end comment section --}}
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Reply</button>
+                </div>
+            </form>
 
-    </div> @endif
+            {{-- end comment section --}}
 
-    
-    @can('update',$post)
-    @if($post->id ==  request()->get('id') )
-    <form action="{{route('posts.update',$post)}}" method="POST" class="mb-4">
-        @method('PATCH')
-        @csrf
-        <div class="mb-4">
-            <label for="body" class="sr-only">Body</label>
-            <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!">{{$post->body}}</textarea>
-            
-            @error('body')
-            <div class="text-red-500 mt-2 text-sm">
-                {{ $message }}
-            </div>
-            @enderror
         </div>
-        
-        <div>
-            <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded font-medium">Update</button>
-            <button class="bg-red-500 text-white px-4 py-2 rounded font-medium"><a href="{{route('posts')}}" class="p-3">Cancel</a></button>
-        </div>
-    </form>
     @endif
+
+
+    @can('update', $post)
+        @if ($post->id == request()->get('id'))
+            <form action="{{ route('posts.update', $post) }}" method="POST" class="mb-4">
+                @method('PATCH')
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="4"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                        placeholder="Post something!">{{ $post->body }}</textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded font-medium">Update</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded font-medium"><a href="{{ route('posts') }}"
+                            class="p-3">Cancel</a></button>
+                </div>
+            </form>
+        @endif
     @endcan
-      
+
 </div>
--- laravel-post/resources/views/layouts/app.blade.php
+++ laravel-post/resources/views/layouts/app.blade.php
@@ -1,61 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>post app</title>
-    
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
-        
-    <ul class="flex items-center">         
 
-        <li>
-            <a href="/" class="p-3">Home</a>
-        </li> 
-        <li>
-            <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
-        </li> 
-        <li>
-            <a href="{{route('posts')}}" class="p-3">Post</a>
-        </li> 
+        <ul class="flex items-center">
 
-    </ul>
-    <ul class="flex items-center">
+            <li>
+                <a href="/" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="{{ route('posts') }}" class="p-3">Post</a>
+            </li>
 
-        @auth
-            
-        <li>
-            <a href="" class="p-3">{{auth()->User()->username}}</a>
-        </li> 
+        </ul>
+        <ul class="flex items-center">
 
-        <li>
-            <form action="{{route('logout')}}" method="POST" class="p-3 inline">
-                @csrf
-                <button class="p-3" type="submit">Logout</button>
-            </form>
-        </li> 
+            @auth
 
-        @endauth
+                <li>
+                    <a href="" class="p-3">{{ auth()->User()->username }}</a>
+                </li>
 
-        @guest
-        <li>
-            <a href="{{route('login')}}" class="p-3">Login</a>
-        </li> 
-        <li>
-            <a href="{{route('register')}}" class="p-3">Register</a>
-        </li> 
-        @endguest
-        
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="p-3 inline">
+                        @csrf
+                        <button class="p-3" type="submit">Logout</button>
+                    </form>
+                </li>
 
-    </ul>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
+
+
+        </ul>
 
     </nav>
     @yield('content')
 </body>
+
 </html>
